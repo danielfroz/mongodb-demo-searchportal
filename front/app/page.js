8fc9059176041c2e347a37740b20add473e2c3cb
Daniel Froz <daniel.froz@mongodb.com>
2025-07-28 15:00:31 -0300

    improved layout

--- front/app/page.js
+++ front/app/page.js
@@ -1,6 +1,7 @@
 'use client'
 
 import * as r from '@/lib/redux';
+import { Fragment } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Container from './Container';
 import List from './List';
@@ -12,25 +13,31 @@ export default function Home() {
   const dispatch = useDispatch()
   const tenant = useSelector(state => state.tenant.tenant)
 
+  function deselect() {
+    dispatch(r.tenantTenant(undefined))
+  }
+
   return (
     <div>
       <nav className='mt-2'>
         <Container>
           <TenantSelector/>
           {tenant &&
-            <div className='flex flex-row gap-2 items-start border rounded p-2'>
-              <div className='p-2 cursor-pointer' onClick={() => dispatch(r.tenantTenant(undefined))}>
+            <Fragment>
+              <div className='border rounded cursor-pointer p-2' onClick={deselect}>
                 {tenant.name}
               </div>
+            <div className='flex flex-row gap-2 items-start border rounded p-2'>
               <div className='flex-1'>
                 <Search/>
               </div>
             </div>
+            </Fragment>
           }
         </Container>
       </nav>
       {tenant &&
-        <main>
+        <main className='mb-2'>
           <Container>
             <List/>
             <SearchPagination/>
